d4e7a2388f6cabd439e9a044a9c0c981e4442f25
Philip Craig <689193+philipcraig@users.noreply.github.com>
2021-06-26 11:54:49 +0100

    unix-like command
    * When binary is invoked with no arguments, read stdin
    * add context on Errors
    * add tests of process module

--- src/process.rs
+++ src/process.rs
@@ -2,25 +2,46 @@ use crate::lexer::Lexer;
 use crate::parser::Parser;
 use crate::token::Kind;
 
-use anyhow::{bail, Context, Result};
-use std::{fs, io, io::Write};
+use anyhow::{Context, Result};
+use std::{
+    fs::File,
+    io::{self, Read, Write},
+};
 
-/// process each filepath in the input Vec
+/// process each filepath in the input Vec, or stdin if empty
 pub fn files(filepaths: Vec<String>) -> Result<()> {
-    if filepaths.is_empty() {
-        bail!("At least one filepath must be specified as a command line argument.");
-    }
+    let stdout = io::stdout();
+    let mut outio = stdout.lock();
 
-    for filepath in filepaths {
-        let text = fs::read_to_string(&filepath)
-            .with_context(|| format!("Failed to read from the file at {}", filepath))?;
-        process_text(&text, &mut io::stdout().lock())?;
+    if filepaths.is_empty() {
+        process_stream(io::stdin(), &mut outio).with_context(|| "while reading stdin")?;
+    } else {
+        for filepath in filepaths {
+            let file = File::open(&filepath)
+                .with_context(|| format!("Failed to open file at {}", filepath))?;
+            process_stream(file, &mut outio)
+                .with_context(|| format!("while reading {}", filepath))?;
+        }
     }
     Ok(())
 }
 
-/// process the input text, writing a printable form of each token,
-/// one token per line onto out. Line numbers and columns are written
+/// process an input stream from inio, reading its text and writing the
+/// processed results to outio
+fn process_stream<R, W>(mut inio: R, outio: W) -> Result<()>
+where
+    R: Read,
+    W: Write,
+{
+    let mut text = String::new();
+    inio.read_to_string(&mut text)?;
+
+    process_text(&text, outio)
+}
+
+/// process an input text, writing a printable form of all tokens except
+/// whitespace, one token per line onto out. Line numbers and columns are
+/// written. Any AST errors are written
 fn process_text<W: Write>(text: &str, mut out: W) -> Result<()> {
     let lexer = Lexer::new(text);
     let tokens = lexer.tokens();
@@ -48,4 +69,29 @@ fn process_text<W: Write>(text: &str, mut out: W) -> Result<()> {
         writeln!(out, "{}", err)?;
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "stream did not contain valid UTF-8")]
+    fn bad_unicode_input_causes_a_panic() {
+        let bad_unicode: &[u8] = &[0x44, 0xEF];
+        process_stream(bad_unicode, io::sink()).unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "Failed to open file at")]
+    fn missing_file_causes_a_panic() {
+        files(vec!["madeupfilename".to_string()]).unwrap();
+    }
+
+    #[test]
+    fn valid_utf8_input_doesnt_panic() {
+        let good_input = "meow";
+        process_text(good_input, io::sink()).unwrap();
+    }
 }
