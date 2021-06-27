94fc0da5dafcafaa61f75ebef094dbfafabd9f8f
Philip Craig <689193+philipcraig@users.noreply.github.com>
2021-06-27 20:34:17 +0100

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
-use std::{env, process};
 use mylang::io::process_files;
+use std::{env, process};
 
 fn main() -> Result<()> {
     process::exit(process_files(env::args().skip(1).collect())? as i32)
