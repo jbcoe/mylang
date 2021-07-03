a0119297fb8a3b86ad53cc506450e0c31064dfa8
Philip Craig <689193+philipcraig@users.noreply.github.com>
2021-07-03 20:15:13 +0100

    * Add StructOpt for parsing * Add possibility of alternate parser (no such parser implemented)  * clippy fixes  * rename driver::go to driver::real_main

--- src/driver.rs
+++ src/driver.rs
@@ -1,3 +1,4 @@
+use crate::ast::AbstractSyntaxTree;
 use crate::evaluator::Evaluator;
 use crate::lexer::Lexer;
 use crate::parser::Parser;
@@ -17,6 +18,10 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "mylang")]
 pub struct Opt {
+    /// Use alternate parser
+    #[structopt(short, long)]
+    alternate: bool,
+
     /// Files to process
     #[structopt(name = "FILE", parse(from_os_str))]
     files: Vec<PathBuf>,
@@ -28,24 +33,24 @@ pub struct Opt {
 ///
 /// Will return Err if files couldn't be opened, or found
 /// or if they could not be lexed, parsed or evaluated
-pub fn go(opt: &Opt) -> Result<i32> {
+pub fn real_main(opt: &Opt) -> Result<i32> {
     let stdout = io::stdout();
     let mut outio = stdout.lock();
 
     if opt.files.is_empty() {
-        process_stream(io::stdin(), &mut outio).with_context(|| "while reading stdin")
+        process_stream(io::stdin(), &mut outio, opt).with_context(|| "while reading stdin")
     } else {
         let filepath = &opt.files[0];
         let file = File::open(&filepath)
             .with_context(|| format!("Failed to open file at {}", filepath.display()))?;
-        process_stream(file, &mut outio)
+        process_stream(file, &mut outio, opt)
             .with_context(|| format!("while reading {}", filepath.display()))
     }
 }
 
 /// process an input stream from inio, reading its text and writing the
 /// processed results to outio
-fn process_stream<R, W>(mut inio: R, outio: W) -> Result<i32>
+fn process_stream<R, W>(mut inio: R, outio: W, opt: &Opt) -> Result<i32>
 where
     R: Read,
     W: Write,
@@ -53,13 +58,31 @@ where
     let mut text = String::new();
     inio.read_to_string(&mut text)?;
 
-    process_text(&text, outio)
+    process_text(outio, &text, opt)
 }
 
 /// process an input text, writing a printable form of all tokens except
 /// whitespace, one token per line onto out. Line numbers and columns are
 /// written. Any AST errors are written
-fn process_text<W: Write>(text: &str, mut out: W) -> Result<i32> {
+fn process_text<W: Write>(mut out: W, text: &str, options: &Opt) -> Result<i32> {
+    let ast = if options.alternate {
+        alternate_lex_parse(&mut out, text)
+    } else {
+        lex_and_parse(&mut out, text)?
+    };
+    let mut evaluator = Evaluator::new();
+    let result = evaluator.evaluate(&ast);
+    for err in evaluator.errors() {
+        writeln!(out, "{}", err)?;
+    }
+    Ok(result)
+}
+
+fn alternate_lex_parse<W: Write>(mut _out: W, _text: &str) -> AbstractSyntaxTree {
+    AbstractSyntaxTree::new(vec![], vec![])
+}
+
+fn lex_and_parse<W: Write>(mut out: W, text: &str) -> Result<AbstractSyntaxTree, anyhow::Error> {
     let lexer = Lexer::new(text);
     let tokens = lexer.tokens();
     for token in &tokens {
@@ -86,38 +109,37 @@ fn process_text<W: Write>(text: &str, mut out: W) -> Result<i32> {
     for err in ast.errors() {
         writeln!(out, "{}", err)?;
     }
-    let mut evaluator = Evaluator::new();
-    let result = evaluator.evaluate(&ast);
-    for err in evaluator.errors() {
-        writeln!(out, "{}", err)?;
-    }
-    Ok(result)
+    Ok(ast)
 }
 
 #[cfg(test)]
 mod tests {
 
+    use structopt::lazy_static::lazy_static;
+
     use super::*;
+
+    lazy_static! {
+        static ref OPTS: Opt = Opt::from_iter(vec!["mylang"]);
+    }
 
     #[test]
     #[should_panic(expected = "stream did not contain valid UTF-8")]
     fn invalid_utf8_input_causes_a_panic() {
         let invalid_utf8: &[u8] = &[0x44, 0xEF];
-        process_stream(invalid_utf8, io::sink()).unwrap();
+        process_stream(invalid_utf8, io::sink(), &OPTS).unwrap();
     }
 
     #[test]
     #[should_panic(expected = "Failed to open file at /dev/null/madeupfile")]
-    fn missing_file_causes_a_panic() {
-        let opts = Opt {
-            files: vec![PathBuf::from(r"/dev/null/madeupfile")],
-        };
-        go(&opts).unwrap();
+    fn unopenable_file_causes_a_panic() {
+        let opts: Opt = Opt::from_iter(vec!["mylang", "/dev/null/madeupfile"]);
+        real_main(&opts).unwrap();
     }
 
     #[test]
     fn valid_utf8_input_doesnt_panic() {
         let valid_utf8: &[u8] = b"let meow; meow";
-        process_stream(valid_utf8, io::sink()).unwrap();
+        process_stream(valid_utf8, io::sink(), &OPTS).unwrap();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
-use mylang::driver::{go, Opt};
+use mylang::driver::{real_main, Opt};
 use std::process;
 use structopt::StructOpt;
 
 fn main() -> Result<()> {
-    process::exit(go(&Opt::from_args())?)
+    process::exit(real_main(&Opt::from_args())?)
 }
