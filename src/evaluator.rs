2841c8851cf771bc623c39288ced0fe1ac2cacc7
Jonathan B Coe <jonathanbcoe@gmail.com>
2021-07-04 09:50:05 +0100

    Improve macros for defining evalautor tests (#76)

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -35,132 +35,102 @@ impl<'a> Evaluator<'a> {
 mod tests {
     use crate::{evaluator::Evaluator, lexer::Lexer, parser::Parser};
 
-    struct EvaluatorTestCase {
-        input: &'static str,
-        return_value: i32,
-    }
-
     macro_rules! evaluator_test_case {
-        ($test_name:ident, $test_case:expr) => {
+        (name: $test_name:ident, input: $input:expr, return_value: $return_value:expr,) => {
             #[test]
             fn $test_name() {
-                let tokens = Lexer::new($test_case.input).tokens();
+                let tokens = Lexer::new($input).tokens();
                 let parser = Parser::new(tokens);
                 let ast = parser.ast();
                 let errors = ast.errors();
 
                 assert!(errors.is_empty(), "Expected no errors, got {:?}", errors);
                 let mut evaluator = Evaluator::new();
-                assert_eq!(evaluator.evaluate(&ast), $test_case.return_value);
+                assert_eq!(evaluator.evaluate(&ast), $return_value);
             }
         };
     }
 
-    evaluator_test_case![
-        return_integer,
-        EvaluatorTestCase {
-            input: "return 42;",
-            return_value: 42,
-        }
-    ];
+    evaluator_test_case! {
+        name: return_integer,
+        input: "return 42;",
+        return_value: 42,
+    }
 
-    evaluator_test_case![
-        let_string_no_return,
-        EvaluatorTestCase {
-            input: r#"let a = "Hello";"#,
-            return_value: 0,
-        }
-    ];
+    evaluator_test_case! {
+        name: let_string_no_return,
+        input: r#"let a = "Hello";"#,
+        return_value: 0,
+    }
 
-    evaluator_test_case![
-        let_float_no_return,
-        EvaluatorTestCase {
-            input: "let a = 3.14159;",
-            return_value: 0,
-        }
-    ];
+    evaluator_test_case! {
+        name: let_float_no_return,
+        input: "let a = 3.14159;",
+        return_value: 0,
+    }
 
-    evaluator_test_case![
-        let_integer_no_return,
-        EvaluatorTestCase {
-            input: "let a = 42;",
-            return_value: 0,
-        }
-    ];
+    evaluator_test_case! {
+        name: let_integer_no_return,
+        input: "let a = 42;",
+        return_value: 0,
+    }
 
-    evaluator_test_case![
-        let_integer_return_identifier,
-        EvaluatorTestCase {
-            input: "let a = 42; return a;",
-            return_value: 42,
-        }
-    ];
+    evaluator_test_case! {
+        name: let_integer_return_identifier,
+        input: r#"
+            let a = 42; 
+            return a;"#,
+        return_value: 42,
+    }
 
-    evaluator_test_case![
-        let_function_no_return,
-        EvaluatorTestCase {
-            input: "let a = func ( x, y ) { return x; };",
-            return_value: 0,
-        }
-    ];
+    evaluator_test_case! {
+        name: let_function_no_return,
+        input: r#"
+            let a = func ( x, y ) { 
+                return x; 
+            };"#,
+        return_value: 0,
+    }
 
-    evaluator_test_case![
-        let_function_return_invocation_with_identifiers,
-        EvaluatorTestCase {
-            input: r#"
-# Define a function.
-let first = func (a, b) { 
-    return a; # indenting is non-syntactic.
-};
-let x = 1;
-let y = 2;
-let z = first(x, y);
-return z;"#,
-            return_value: 1,
-        }
-    ];
+    evaluator_test_case! {
+        name: let_function_return_invocation_with_identifiers,
+        input: r#"
+            # Define a function.
+            let first = func (a, b) { 
+                return a; # indenting is non-syntactic.
+            };
+            let x = 1;
+            let y = 2;
+            let z = first(x, y);
+            return z;"#,
+        return_value: 1,
+    }
 
-    evaluator_test_case![
-        let_function_return_invocation_with_literals,
-        EvaluatorTestCase {
-            input: r#"
-let first = func (a, b) { 
-    return a; 
-};
-return first(101, 2);"#,
-            return_value: 101,
-        }
-    ];
+    evaluator_test_case! {
+        name: let_function_return_invocation_with_literals,
+        input: r#"
+            let first = func (a, b) { 
+                return a; 
+            };
+            return first(101, 2);"#,
+        return_value: 101,
+    }
 
-    evaluator_test_case![
-        assorted_let_with_return_integer,
-        EvaluatorTestCase {
-            input: r#"let a = 42; let b = "Hello"; let c = 3.14159; return 7;"#,
-            return_value: 7,
-        }
-    ];
+    evaluator_test_case! {
+        name: assorted_let_with_return_integer,
+        input: r#"
+            let a = 42; 
+            let b = "Hello"; 
+            let c = 3.14159; 
+            return 7;"#,
+        return_value: 7,
+    }
 
-    evaluator_test_case![
-        assorted_let_with_return_identifier,
-        EvaluatorTestCase {
-            input: r#"let a = 42; a;"#,
-            return_value: 0, // not 42
-        }
-    ];
-
-    evaluator_test_case![
-        function_call_returns_true,
-        EvaluatorTestCase {
-            input: r#"let f = func() { return True;}; let a = f();"#,
-            return_value: 0,
-        }
-    ];
-
-    evaluator_test_case![
-        function_call_returns_false,
-        EvaluatorTestCase {
-            input: r#"let f = func() { return False;}; let a = f();"#,
-            return_value: 0,
-        }
-    ];
+    evaluator_test_case! {
+        name: assorted_let_with_return_identifier,
+        input: r#"
+            let a = 42; 
+            a;"#,
+        return_value: 0, // not 42
+    }
 }
